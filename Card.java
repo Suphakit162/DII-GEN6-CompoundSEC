d38a2815e7abf74e0f258ef002fef390b1ce79b7
Suphakit162 <supakita1234@gmail.com>
2025-02-05 22:03:18 +0700

    Edit 5

--- Card.java
+++ Card.java
@@ -1,25 +1,60 @@
-import java.util.*;
-public class Card {
-    private String name;
-    private int age;
-    private int id;
-    private String positive;
-    private boolean check;
 
-    public Card (String name,int age,int id,String positive) {
-        this.name = name ;
-        this.age = age ;
-        this.id = id ;
-        this.positive = positive ;
+public abstract class Card {
+    protected String name;
+    protected int age;
+    protected int id;
+    protected String positive;
+    protected boolean check;
+
+
+    public Card(String name, int age, int id, String positive) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.positive = positive;
+        this.check = false;
     }
 
-    public boolean isCheck() {
-        return check;
+
+    public abstract void validateCard();
+
+
+    public String getDetails() {
+        return "Name: " + name + ", Age: " + age + ", ID: " + id + ", Positive: " + positive;
+    }
+}
+
+// บัตรพนักงาน
+class EmployeeCard extends Card {
+    public EmployeeCard(String name, int age, int id, String positive) {
+        super(name, age, id, positive);
     }
 
     @Override
-    public String toString() {
-        return "Card{name='" + name + "', age=" + age + ", id=" + id + ", positive='" + positive + "'}";
+    public void validateCard() {
+        if (age >= 18) {
+            check = true;
+            System.out.println("Employee Card " + name + " is valid.");
+        } else {
+            System.out.println("Employee Card " + name + " is invalid.");
+        }
+    }
+}
+
+// บัตรผู้เยี่ยมชม
+class VisitorCard extends Card {
+    public VisitorCard(String name, int age, int id, String positive) {
+        super(name, age, id, positive);
     }
 
+    @Override
+    public void validateCard() {
+        if (age >= 12) {
+            check = true;
+            System.out.println("Visitor Card " + name + " is valid.");
+        } else {
+            System.out.println("Visitor Card " + name + " is invalid.");
+        }
+    }
 }
+
